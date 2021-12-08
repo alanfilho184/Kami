83ab929bbfae01901bd7a136b67e79003726a3bf
Kamikaze_184 <alan.filho@hotmail.com>
2021-12-07 23:30:22 -0300

    v4.0.1 - Testes

--- system/commands/owner/reloadSlash.js
+++ system/commands/owner/reloadSlash.js
@@ -30,166 +30,163 @@ module.exports = class reloadSlash {
         }
     }
 
-    execute(client, int) {
-        int.deferReply({ ephemeral: true })
-            .then(async () => {
-                const rest = new REST({ version: '9' }).setToken(client.settings.token);
+    async execute(client, int) {
+        const rest = new REST({ version: '9' }).setToken(client.settings.token);
 
-                const slashs = await rest.get(
-                    Routes.applicationCommands(client.settings.clientId)
-                )
+        const slashs = await rest.get(
+            Routes.applicationCommands(client.settings.clientId)
+        )
 
-                const commands = new Array()
-                const commandsOwner = new Array()
+        const commands = new Array()
+        const commandsOwner = new Array()
 
-                client.commands.forEach(c => {
-                    if (c.type == 1) {
-                        const command = new SlashCommandBuilder()
+        client.commands.forEach(c => {
+            if (c.type == 1) {
+                const command = new SlashCommandBuilder()
 
-                        if (c.ownerOnly) {
-                            command.setDefaultPermission(false)
-                            command.setName(c.name).setDescription(c.desc)
+                if (c.ownerOnly) {
+                    command.setDefaultPermission(false)
+                    command.setName(c.name).setDescription(c.desc)
 
-                            if (c.args.length > 0) {
-                                for (var x in c.args) {
-                                    if (c.args[x].type == "STRING") {
-                                        command.addStringOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                    else if (c.args[x].type == "INTEGER") {
-                                        command.addIntegerOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                    else if (c.args[x].type == "USER") {
-                                        command.addUserOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                }
+                    if (c.args.length > 0) {
+                        for (var x in c.args) {
+                            if (c.args[x].type == "STRING") {
+                                command.addStringOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
                             }
-
-                            if (c.options.length > 0) {
-                                for (var x in c.options) {
-                                    if (c.options[x].type == "STRING") {
-                                        command.addStringOption(option => {
-                                            option.setName(c.options[x].name).setRequired(c.options[x].required).setDescription(c.options[x].desc)
-
-                                            if (c.options[x].choices.length > 0) {
-                                                for (var y in c.options[x].choices) {
-                                                    option.addChoice(c.options[x].choices[y].name, c.options[x].choices[y].return)
-                                                }
-                                            }
-
-                                            return option
-                                        })
-                                    }
-                                }
+                            else if (c.args[x].type == "INTEGER") {
+                                command.addIntegerOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
                             }
-
-                            commandsOwner.push(command)
-
-                        }
-                        else {
-                            command.setDefaultPermission(true)
-                            command.setName(c.name).setDescription(c.desc)
-
-                            for (x in slashs) {
-                                if (slashs[x].name == c.name) {
-                                    command.id = slashs[x].id
-                                }
+                            else if (c.args[x].type == "USER") {
+                                command.addUserOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
                             }
-
-                            if (c.args.length > 0) {
-                                for (var x in c.args) {
-                                    if (c.args[x].type == "STRING") {
-                                        command.addStringOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                    else if (c.args[x].type == "INTEGER") {
-                                        command.addIntegerOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                    else if (c.args[x].type == "USER") {
-                                        command.addUserOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
-                                    }
-                                }
-                            }
-
-                            if (c.options.length > 0) {
-                                for (var x in c.options) {
-                                    if (c.options[x].type == "STRING") {
-                                        command.addStringOption(option => {
-                                            option.setName(c.options[x].name).setDescription(c.options[x].desc).setRequired(c.options[x].required)
-
-                                            if (c.options[x].choices.length > 0) {
-                                                for (var y in c.options[x].choices) {
-                                                    option.addChoice(c.options[x].choices[y].name, c.options[x].choices[y].return)
-                                                }
-                                            }
-
-                                            return option
-                                        })
-                                    }
-                                }
-                            }
-
-                            commands.push(command)
                         }
                     }
-                    else if (c.type == 3) {
-                        const command = new ContextMenuCommandBuilder()
 
-                        if (c.ownerOnly) {
-                            command.setDefaultPermission(false)
-                            command.setName(c.name)
-                            command.setType(c.type)
+                    if (c.options.length > 0) {
+                        for (var x in c.options) {
+                            if (c.options[x].type == "STRING") {
+                                command.addStringOption(option => {
+                                    option.setName(c.options[x].name).setRequired(c.options[x].required).setDescription(c.options[x].desc)
 
-                            commandsOwner.push(command)
-                        }
-                        else {
-                            command.setDefaultPermission(true)
-                            command.setName(c.name)
-                            command.setType(c.type)
+                                    if (c.options[x].choices.length > 0) {
+                                        for (var y in c.options[x].choices) {
+                                            option.addChoice(c.options[x].choices[y].name, c.options[x].choices[y].return)
+                                        }
+                                    }
 
-                            commands.push(command)
+                                    return option
+                                })
+                            }
                         }
                     }
+
+                    commandsOwner.push(command)
+
+                }
+                else {
+                    command.setDefaultPermission(true)
+                    command.setName(c.name).setDescription(c.desc)
+
+                    for (x in slashs) {
+                        if (slashs[x].name == c.name) {
+                            command.id = slashs[x].id
+                        }
+                    }
+
+                    if (c.args.length > 0) {
+                        for (var x in c.args) {
+                            if (c.args[x].type == "STRING") {
+                                command.addStringOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
+                            }
+                            else if (c.args[x].type == "INTEGER") {
+                                command.addIntegerOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
+                            }
+                            else if (c.args[x].type == "USER") {
+                                command.addUserOption(option => option.setName(c.args[x].name).setDescription(c.args[x].desc).setRequired(c.args[x].required))
+                            }
+                        }
+                    }
+
+                    if (c.options.length > 0) {
+                        for (var x in c.options) {
+                            if (c.options[x].type == "STRING") {
+                                command.addStringOption(option => {
+                                    option.setName(c.options[x].name).setDescription(c.options[x].desc).setRequired(c.options[x].required)
+
+                                    if (c.options[x].choices.length > 0) {
+                                        for (var y in c.options[x].choices) {
+                                            option.addChoice(c.options[x].choices[y].name, c.options[x].choices[y].return)
+                                        }
+                                    }
+
+                                    return option
+                                })
+                            }
+                        }
+                    }
+
+                    commands.push(command)
+                }
+            }
+            else if (c.type == 3) {
+                const command = new ContextMenuCommandBuilder()
+
+                if (c.ownerOnly) {
+                    command.setDefaultPermission(false)
+                    command.setName(c.name)
+                    command.setType(c.type)
+
+                    commandsOwner.push(command)
+                }
+                else {
+                    command.setDefaultPermission(true)
+                    command.setName(c.name)
+                    command.setType(c.type)
+
+                    commands.push(command)
+                }
+            }
+        })
+
+        try {
+            await rest.put(
+                Routes.applicationCommands(client.settings.clientId),
+                { body: commands },
+            )
+
+        } catch (error) {
+            client.log.error(error, true)
+        }
+
+        try {
+            await rest.put(
+                Routes.applicationGuildCommands(client.settings.clientId, "717173630110400515"),
+                { body: commandsOwner },
+            )
+                .then(async r => {
+                    var commands = await client.guilds.cache.get("717173630110400515").commands.fetch()
+
+                    commands.forEach(async c => {
+
+                        const command = await client.guilds.cache.get("717173630110400515").commands.fetch(c.id);
+
+                        const permissions = [
+                            {
+                                id: client.settings.owner,
+                                type: 'USER',
+                                permission: true,
+                            },
+                        ];
+
+                        await command.permissions.add({ permissions })
+                    })
                 })
 
-                try {
-                    await rest.put(
-                        Routes.applicationCommands(client.settings.clientId),
-                        { body: commands },
-                    )
+        } catch (error) {
+            client.log.error(error, true)
+        }
 
-                } catch (error) {
-                    client.log.error(error, true)
-                }
+        int.reply("Slash commands recarregados")
 
-                try {
-                    await rest.put(
-                        Routes.applicationGuildCommands(client.settings.clientId, "717173630110400515"),
-                        { body: commandsOwner },
-                    )
-                        .then(async r => {
-                            var commands = await client.guilds.cache.get("717173630110400515").commands.fetch()
-
-                            commands.forEach(async c => {
-
-                                const command = await client.guilds.cache.get("717173630110400515").commands.fetch(c.id);
-
-                                const permissions = [
-                                    {
-                                        id: client.settings.owner,
-                                        type: 'USER',
-                                        permission: true,
-                                    },
-                                ];
-
-                                await command.permissions.add({ permissions })
-                            })
-                        })
-
-                } catch (error) {
-                    cclient.log.error(error, true)
-                }
-
-                int.editReply("Slash commands recarregados")
-
-            })
     }
 }
