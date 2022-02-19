0ba337dafd74deaf5af0a43675af9a835fcd5dac
Kamikaze_184 <alan.filho@hotmail.com>
2022-02-18 22:26:53 -0300

    v4.4.5 - Endpoint para postar o log completo do site no Discord

--- system/modules/api/services/api/api.services.js
+++ system/modules/api/services/api/api.services.js
@@ -1,25 +1,17 @@
 const pass = new Object()
+const fs = require("fs")
 
 module.exports = class apiServices {
     constructor(client) {
         pass.client = client
     }
 
-    log(info) {
-        if (info.format == "msg") {
-            if (info.type == "info") {
-                pass.client.log.info(info.msg)
-            }
-            else if (info.type == "error") {
-                pass.client.log.error(info.msg, info.ping)
-            }
-            else if (info.type == "warn") {
-                pass.client.log.warn(info.msg, info.ping)
-            }
-        }
-        else if (info.format == "embed") {
-            pass.client.log.embed(info.embed, info.ping)
-        }
+    log(body) {
+        pass.client.channels.fetch(pass.client.settings.log)
+            .then(c => {
+                fs.writeFileSync(`siteLog.txt`, body.log)
+                c.send({ content: body.content, files: [`siteLog.txt`] })
+            })
     }
 
     isBeta(body) {
