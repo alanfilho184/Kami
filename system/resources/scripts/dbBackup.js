cc256424fb1ed9c166e81fe505d40c2af5f1ca3e
Alan Leal <alan.filho@hotmail.com>
2022-03-18 14:41:42 -0300

    v4.7.2 - dbBackup em desenvolvimento

--- system/resources/scripts/dbBackup.js
+++ system/resources/scripts/dbBackup.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const path = require("path")
 
 module.exports = class psql_backup {
-    constructor(db) {
+    constructor({ db, options }) {
         this.db = db
+        this.options = options
     }
 
     toJSON(options = { backupPath: path.parse("/"), backupName: "dbBackup", schema: "public" }) {
@@ -14,6 +15,7 @@ module.exports = class psql_backup {
                 tables.forEach((table) => {
                     this.db.query(`SELECT * FROM ${table}`)
                         .then((res) => {
+                            //TODO Salvar tipos e tamanho das colunas
                             backup[table] = new Array()
 
                             res[0].forEach(async (item) => {
@@ -32,7 +34,30 @@ module.exports = class psql_backup {
             })
     }
 
-    toPSQL() {
+    toPSQLScript() {
+        var sqlScript = ""
 
+        let backup = fs.readFileSync(this.options.backupPath.toString() + this.options.backupName, { encoding: "utf-8" })
+        backup = JSON.parse(backup)
+
+        //TODO loop para criar tabelas
+
+        Object.keys(backup).forEach(table => {
+            sqlScript += `INSERT INTO ${TABLE} (`
+
+            Object.keys(backup[table]).forEach(key => {
+                sqlScript += `${key},`
+            })
+
+            sqlScript.substring(sqlScript.length, sqlScript.length - 1)
+            sqlScript += ")\n VALUES ("
+
+            Object.values(backup[table]).forEach(value => {
+                sqlScript += `${value},`
+            })
+
+            sqlScript.substring(sqlScript.length, sqlScript.length - 1)
+            sqlScript += ")"
+        })
     }
 }
