98f2522e8a6e6481748ddfe73c0fefa22c51df0f
alanfilho184 <alan.filho@hotmail.com>
2022-05-30 23:50:24 -0300

    v4.7.11 - Opções de inicialização

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ require('dotenv').config()
 
 const Client = require('./system/modules/Client')
 const options = require('./settings')
+const args = new Array()
 
-const client = new Client(options)
+process.argv.forEach(function (arg) {
+    if (arg.startsWith('--')) {
+        args.push(arg.substring(2))
+    }
+});
 
-client.login()
+if (args.includes('full')) {
+    options.full = true
+    const client = new Client(options)
+    client.login()
+}
+else if (args.includes('api')) {
+    options.api = true
+    new Client(options)
+}
+else if (args.includes('bot')) {
+    options.bot = true
+    const client = new Client(options)
+    client.login()
+}
+else {
+    options.full = true
+    const client = new Client(options)
+    client.login()
+}
