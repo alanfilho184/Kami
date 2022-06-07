05addfc3a1b3eace0d84dd541173f9ae27e8ec12
alanfilho184 <alan.filho@hotmail.com>
2022-06-07 10:47:36 -0300

    v4.7.12 - Lendo configurações

--- system/events/discord/ready.js
+++ system/events/discord/ready.js
@@ -9,6 +9,7 @@ module.exports = {
         client.emit("activity", "start")
         client.channels.cache.get("784035696414425108").messages.fetch("784039521645101066")
             .then((msg) => {
+                msg.content = msg.content.split("<@720128587289722902> <@716053210179043409>").join("")
                 msg.content = msg.content.split("```js").join("")
                 msg.content = msg.content.split("```").join("")
                 var msgConfig = JSON.parse(msg.content)
